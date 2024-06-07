d7a2009c280c52cad8eb823e7c5dfe0050791614
MaddyM05 <134800087+MaddyM05@users.noreply.github.com>
2024-06-07 10:23:25 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   api: {
     server: 'http://localhost:8000/',
-    mapKey: '',
+    mapKey: 'bf738e04-823f-43e6-a06d-64e52a9078b4',
     googleAuthClientId: '',
-    webSocketUrl: ''
+    webSocketUrl: 'ws://http://34.199.41.141:8000/websocket'
   }
 };
